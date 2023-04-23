473a854429232f66ddc713234b2ebf9cd36655af
Destiny Saturday <yiradesat@gmail.com>
2023-04-23 22:00:08 +0200

    added strcmp and fixed bety errors
    Co-authored-by: K NKUATSANA <nkuatsanakeneuoe@gmail.com>

--- read_command.c
+++ read_command.c
@@ -43,7 +43,24 @@ char *read_command(void)
 			prompt2();
 		}
 		ptrlen += linesize;
+	}#include "shell.h"
+
+/**
+* print_env - prints the environment string to stdout
+*
+* Return: 0
+*/
+int print_env(void)
+{
+	int x;
+
+	for (x = 0; environ[x] != NULL; x++)
+	{
+		write(STDOUT_FILENO, environ[x], strlen(environ[x]));
+		write(STDOUT_FILENO, "\n", 1);
 	}
+	return (0);
+}
 
 	return (command);
 }
